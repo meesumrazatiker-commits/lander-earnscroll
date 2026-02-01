902d409bc604551fc9758e40ece24b0b86c0102c
meesumrazatiker-commits <meesumrazatiker@gmail.com>
2026-02-02 02:19:46 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import EarningTicker from "@/components/EarningTicker";
-import { Analytics } from "@vercel/analytics/next";
 
 export default function Home() {
   return (
